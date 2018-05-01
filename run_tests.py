298bcac84e5a30d2d3ad60e11a1b59e087f37d5e
Derick Wasswa <wasswadero@gmail.com>
2018-05-01 22:36:04 +0300

    Add other test coverage badges

--- run_tests.py
+++ run_tests.py
@@ -1,14 +1,20 @@
-from flask import Flask, session, request, url_for, jsonify, make_response, current_app
-from flask_api import FlaskAPI, status
+import os
+import unittest
+from flask import Flask, current_app
+from flask_api import FlaskAPI
 
+# initialize the app with all its configurations
 app = FlaskAPI(__name__, instance_relative_config=True)
+app(config_name=os.getenv('APP_SETTINGS'))
 
-def __init__(self):
-    import unittest
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
     tests = unittest.TestLoader().discover('v1', pattern='test_app.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
+    
     return not result.wasSuccessful()
-
 
 if __name__ == '__main__':
     app.run()
