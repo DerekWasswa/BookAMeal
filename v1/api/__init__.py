df02248438d4244a6a9ff2a509220b449a4bfe14
Derick Wasswa <wasswadero@gmail.com>
2018-05-10 10:38:16 +0300

    [feature] initialised sqlalchemy to the app instances

--- v1/api/__init__.py
+++ v1/api/__init__.py
@@ -2,13 +2,17 @@
 from flask import Flask, session, render_template, request, redirect, url_for, jsonify, make_response, current_app
 from flask_api import FlaskAPI, status
 import json
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
     # Initialize the Flask application
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = "boOk-a-MeAL"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/book_a_meal_db'
     asyncMode = None
+
+    db = SQLAlchemy(app)
 
     from v1.api.endpoints import endpoints_blueprint
     app.register_blueprint(endpoints_blueprint, url_prefix='/api/v1')
