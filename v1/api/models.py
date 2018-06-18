b0120bdb0e02adcb3ff724b36def6ccb695cfd38
Wasswa Derick <wasswaderick@MacBook-Pro-5.local>
2018-06-18 15:07:45 +0300

    Remove unnecessary/irrelevant code from Models

--- v1/api/models.py
+++ v1/api/models.py
@@ -28,25 +28,11 @@ class User(db.Model):
         user['username'] = self.username
         user['email'] = self.email
         user['admin'] = self.admin
-        return user
-
-    #check if a certain user object exists in the app users
-    @staticmethod
-    def check_exists(user_email):
-        for user in app_users:
-            if user.email == user_email:
-                return True
-        return False  
+        return user 
 
     #Check if the password the user is submitting matches the one they registered with
     def verify_user_password(self, user_password):
-        return check_password_hash(self.password, user_password)    
-
-    def get_user_by_email(self, user_email):
-        for user in app_users:
-            if user.email == user_email:
-                return user
-        return None  
+        return check_password_hash(self.password, user_password)     
 
     def __repr__(self):
         return "<User(user_id = '%s', username ='%s', password='%s', email='%s', admin='%s')>" % (self.user_id, self.username, self.password, self.email, self.admin)
@@ -72,51 +58,9 @@ class Meal(db.Model):
         db.session.add(self)
         db.session.commit()
 
-    def add_meal(self):
-        meal = {}
-        meal['meal_id'] = self.meal_id
-        meal['meal'] = self.meal
-        meal['price'] = self.price
-        app_meals.append(meal)
-
     @staticmethod
     def delete_meal():
-        db.session.commit() 
-
-    @staticmethod
-    def update_meal_by_id(mealId, meal_update):
-        for meal in app_meals:
-            if meal['meal_id'] == mealId:
-                price = meal['price']
-                app_meals.remove(meal)
-                new_meal = {}
-                new_meal['meal_id'] = mealId
-                new_meal['meal'] = meal_update
-                new_meal['price'] = price
-                app_meals.append(new_meal)
-                break
-
-    @staticmethod
-    def check_if_meal_exists(meal):
-        for i in range(len(app_meals)):
-            if str(app_meals[i]['meal']) == str(meal):
-                return True
-        return False  
-
-    @staticmethod
-    def delete_meal_by_id(mealId):
-        for i in range(len(app_meals)):
-            if str(app_meals[i]['meal_id']) == str(mealId):
-                del app_meals[i]
-                return True
-        return False     
-    
-    @staticmethod
-    def get_meal_by_id(meal_id):
-        for meal in app_meals:
-            if meal_id == meal['meal_id']:
-                return meal
-        return None      
+        db.session.commit()      
 
     def get_meal_as_dict(self):
         meal_as_dict = {}
@@ -153,19 +97,6 @@ class Menu(db.Model):
         self.meals = []
         self.description = description
         self.vendor_id = vendor_id
-        
-    @staticmethod    
-    def add_meal_to_menu(meal_object, date, menu_object):
-        meal = {}
-        meal['meal_id'] = meal_object['meal_id']
-        meal['meal'] = meal_object['meal']
-        meal['price'] = meal_object['price']
-
-        #Add Meal to the date's menu
-        for menu in app_menu:
-            if date == menu['date']:
-                menu_object.meals.append(meal)
-                break
         
     @staticmethod
     def add_meals_to_menu():
@@ -243,19 +174,5 @@ class Order(db.Model):
         order_as_dict['date'] = order.date
         return order_as_dict
 
-    def make_order(self):
-        order = {}
-        order['order_id'] = self.order_id
-        order['user_id'] = self.user_id
-        order['meal_id'] = self.order_id
-        app_orders.append(order)
-
-    @staticmethod
-    def update_order_by_id(order_id, order_to_update):
-        for i in range(len(app_orders)):
-            if str(app_orders[i]['order_id']) == str(order_id):
-                app_orders[i]["meal_id"] = order_to_update
-                return True
-        return False 
 
          
