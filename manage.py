4dff07850a9957c3925516a2c4d216730066ceba
wasswa-derick <derick.wasswa@andela.com>
2018-07-11 13:31:13 +0300

    [Chore 158965102] Correct procfile

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ import unittest
 from flask_script import Manager, Shell
 from flask import Flask
 from v1.api import create_app, db
+from v1.api.models.users import User
+from v1.api.models.meals import Meal
+from v1.api.models.menu import Menu
+from v1.api.models.orders import Order
 from flask_migrate import Migrate, MigrateCommand
 
 
@@ -11,6 +15,10 @@ app = create_app('production')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+def shell_context():
+    return dict(app=app, User=User, Meal=Meal, Menu=Menu, db=db, Order=Order)
+
+manager.add_command("shell", Shell(make_context=shell_context))
 manager.add_command("db", MigrateCommand)
 
 @manager.command
